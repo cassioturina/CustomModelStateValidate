9ef4f83c3e5673148fc345c87a44a7c33c6c12a5
Cassio Turina <cassioturina.net@gmail.com>
2021-08-19 15:14:18 -0300

    Add project files.

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,26 @@
+﻿using CustomModelStateValidate.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CustomModelStateValidate.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientesController : ControllerBase
+    {
+
+        private readonly ILogger<ClientesController> _logger;
+
+        public ClientesController(ILogger<ClientesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] ClienteModel model)
+        {
+            //model sendo validada na startup.cs
+            return Ok(model);
+        }
+    }
+}
---
+++ Models/ClienteModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomModelStateValidate.Models
+{
+    public class ClienteModel
+    {
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(120, ErrorMessage = "{0} deve ter entre {2} e {1} caracteres", MinimumLength = 3)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatória")]
+        [Range(6,18 , ErrorMessage = "{0} deve ser entre {1} e {2} anos")]
+        public int Idade { get; set; }
+    }
+}
